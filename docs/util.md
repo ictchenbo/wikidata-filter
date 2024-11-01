a2de9d7e64a72b2336da3807cd7e83f0876814a7
chenbo1989 <chenbo01@ict.ac.cn>
2024-11-01 16:03:06 +0800

    add docs

--- docs/util.md
+++ docs/util.md
@@ -18,3 +18,15 @@
 1. 转为中文简体 `util.lang_util.zh_simple(s)`
 
 ### 文件
+1. 读取文本文件内容 `util.files.read_text(filename, encoding='utf8')`
+
+### 日期时间
+1. 当前时间（1970.1.1以来的秒） `util.dates.current()`
+2. 当前时间（1970.1.1以来的毫秒） `util.dates.current_ts()`
+3. 当前日期（形式：2024-11-01） `util.dates.current_date(fmt='%Y-%m-%d')`
+4. 当前时间（形式：2024-11-01 15:50:00） `util.dates.current_time(fmt='%Y-%m-%d %H:%M:%S')`
+5. 字符串日期转时间戳（1970.1.1以来的秒） `util.dates.date(date, fmt='%Y-%m-%d')`
+6. 字符串日期转时间戳（1970.1.1以来的毫秒） `util.dates.date_ts(date,fmt='%Y-%m-%d')`
+7. 日期格式化 `util.dates.ts2date(ts, fmt='%Y-%m-%d')` 输入为秒
+8. 时间格式化 `util.dates.ts2datetime(ts, fmt='%Y-%m-%d')` 输入为秒
+
--- wikidata_filter/util/dates.py
+++ wikidata_filter/util/dates.py
@@ -10,15 +10,19 @@ MONTH_DAYS = [
 YEAR_DAYS = [365, 366]
 
 
-def date2ts(date: str) -> int:
-    return int(time.mktime(time.strptime(date, '%Y-%m-%d')))
+def date(dt: str, fmt='%Y-%m-%d') -> int:
+    return int(time.mktime(time.strptime(dt, fmt)))
 
 
-def ts2date(ts: int, fmt='%Y-%m-%d') -> str:
+def date_ts(dt: str, fmt='%Y-%m-%d') -> int:
+    return int(time.mktime(time.strptime(dt, fmt))*1000)
+
+
+def ts2date(ts, fmt='%Y-%m-%d') -> str:
     return time.strftime(fmt, time.localtime(ts))
 
 
-def ts2datetime(ts: int, fmt='%Y-%m-%d %H:%M:%S') -> str:
+def ts2datetime(ts, fmt='%Y-%m-%d %H:%M:%S') -> str:
     if ts is not None:
         return time.strftime(fmt, time.localtime(ts))
     return ""
@@ -55,15 +59,15 @@ def expand_date_range(dt: str):
     parts = [p for p in parts if p]
     # print(parts)
     if len(parts) == 3:
-        ts = date2ts('-'.join(parts))
+        ts = date('-'.join(parts))
         return ts, ts + ONE_DAY
     if len(parts) == 2:
-        ts = date2ts(f'{parts[0]}-{parts[1]}-01')
+        ts = date(f'{parts[0]}-{parts[1]}-01')
         days = month_days(int(parts[0]), int(parts[1]))
         return ts, ts + ONE_DAY * days
     if len(parts) == 1:
         year = parts[0]
-        ts = date2ts(f'{year}-01-01')
+        ts = date(f'{year}-01-01')
         return ts, ts + ONE_DAY * YEAR_DAYS[is_leap_year(int(year))]
     raise Exception("Invalid date")
 
